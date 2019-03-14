be76c310656897f1aa31d4ee0f027ca11593fa9b
Luis Samano <martindjt@hotmail.com>
2019-03-14 08:04:28 -0600

    finish examen

--- examen.py
+++ examen.py
@@ -5,6 +5,36 @@ import json
 app = Flask(__name__)
 
 
+@app.route('/buscar', methods=['GET'])
+def buscar():
+    params = request.get_json()
+    if params['usuario'] == "admin" and params['contra'] == "123":
+        resp = {
+            'estatus': True,
+            'mensaje': 'Encontrado'
+        }
+    else:
+        resp = {
+            'estatus': False,
+            'mensaje': 'No Encontrado'
+        }
+    return json.dumps(resp)
+
+
+@app.route('/insertar', methods=['POST'])
+def insertar():
+    params = request.get_json()
+    if params['usuario'] == "" and params['contra'] == "":
+        resp = {
+            'mensaje': 'No es posible insertar'
+        }
+    else:
+        resp = {
+            'mensaje': 'Registro insertado'
+        }
+    return json.dumps(resp)
+
+
 @app.route('/modificar', methods=['PUT'])
 def modificar():
     params = request.get_json()
--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,38 @@
 import json
 import http.client
 
+
+""" GET """
+
+
+def get():
+    conn = http.client.HTTPConnection("localhost", 5000)
+    headers = {'Content-type': 'application/json'}
+    body = json.dumps({'usuario': 'admin', 'contra': '123'})
+    conn.request("GET", "/buscar", body, headers)
+    response = conn.getresponse()
+    return response.read().decode()
+
+
+""" POST """
+
+
+def post():
+    conn = http.client.HTTPConnection("localhost", 5000)
+    headers = {'Content-type': 'application/json'}
+    body = json.dumps({'usuario': 'admin', 'contra': '123'})
+    conn.request("POST", "/insertar", body, headers)
+    response = conn.getresponse()
+    return response.read().decode()
+
+
 """ PUT """
 
 
 def put():
     conn = http.client.HTTPConnection("localhost", 5000)
     headers = {'Content-type': 'application/json'}
-    body = json.dumps({'usuario': 'd', 'contra': 'r'})
+    body = json.dumps({'usuario': 'admin', 'contra': '123'})
     conn.request("PUT", "/modificar", body, headers)
     response = conn.getresponse()
     return response.read().decode()
@@ -19,7 +44,7 @@ def put():
 def patch():
     conn = http.client.HTTPConnection("localhost", 5000)
     headers = {'Content-type': 'application/json'}
-    body = json.dumps({'usuario': 'ss', 'contra': '123'})
+    body = json.dumps({'usuario': 'luis', 'contra': 'password'})
     conn.request("PATCH", "/actualizar", body, headers)
     response = conn.getresponse()
     return response.read().decode()
@@ -31,11 +56,17 @@ def patch():
 def delete():
     conn = http.client.HTTPConnection("localhost", 5000)
     headers = {'Content-type': 'application/json'}
-    body = json.dumps({'usuario': 's', 'contra': '123'})
+    body = json.dumps({'usuario': 'root', 'contra': 'root'})
     conn.request("DELETE", "/eliminar", body, headers)
     response = conn.getresponse()
     return response.read().decode()
 
+
+print("\n********** GET ************\n")
+print(get())
+
+print("\n********** POST ************\n")
+print(post())
 
 print("\n********** PUT ************\n")
 print(put())
